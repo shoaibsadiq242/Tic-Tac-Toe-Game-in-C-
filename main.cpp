0a13da35fe1cae9f4b17968e4f9a3c7982ae50a5
Muhammad Shoaib Sadiq Awan <shoaibsadiq.242@gmail.com>
2026-02-08 20:46:14 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+using namespace std;
+
+char board[3][3] = {
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'}
+};
+
+char currentPlayer = 'X';
+
+void displayBoard() {
+    cout << "\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << " " << board[i][j] << " ";
+            if (j < 2) cout << "|";
+        }
+        cout << "\n";
+        if (i < 2) cout << "---+---+---\n";
+    }
+    cout << "\n";
+}
+
+bool placeMark(int choice) {
+    int row = (choice - 1) / 3;
+    int col = (choice - 1) % 3;
+
+    if (board[row][col] != 'X' && board[row][col] != 'O') {
+        board[row][col] = currentPlayer;
+        return true;
+    }
+    return false;
+}
+
+bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == currentPlayer &&
+            board[i][1] == currentPlayer &&
+            board[i][2] == currentPlayer)
+            return true;
+
+        if (board[0][i] == currentPlayer &&
+            board[1][i] == currentPlayer &&
+            board[2][i] == currentPlayer)
+            return true;
+    }
+
+    if (board[0][0] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][2] == currentPlayer)
+        return true;
+
+    if (board[0][2] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][0] == currentPlayer)
+        return true;
+
+    return false;
+}
+
+bool checkDraw() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] != 'X' && board[i][j] != 'O')
+                return false;
+    return true;
+}
+
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+void resetBoard() {
+    char num = '1';
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            board[i][j] = num++;
+}
+
+int main() {
+    char playAgain = 'Y';
+
+    while (playAgain == 'Y' || playAgain == 'y') {
+        resetBoard();
+        currentPlayer = 'X';
+        bool gameOver = false;
+
+        while (!gameOver) {
+            displayBoard();
+            int choice;
+            cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+            cin >> choice;
+
+            if (choice < 1 || choice > 9 || !placeMark(choice)) {
+                cout << "❌ Invalid move. Try again.\n";
+                continue;
+            }
+
+            if (checkWin()) {
+                displayBoard();
+                cout << "🎉 Player " << currentPlayer << " wins!\n";
+                gameOver = true;
+            }
+            else if (checkDraw()) {
+                displayBoard();
+                cout << "😐 It's a draw!\n";
+                gameOver = true;
+            }
+            else {
+                switchPlayer();
+            }
+        }
+
+        cout << "\nDo you want to play again? (Y/N): ";
+        cin >> playAgain;
+    }
+
+    cout << "\nThanks for playing! Press Enter to exit...";
+    cin.ignore();
+    cin.get();
+    return 0;
+}
